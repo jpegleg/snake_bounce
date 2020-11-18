fdae25b15139af5522bf7c8a81279c325653eab5
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2020-11-18 01:44:56 -0700

    Update snake_bounce.py

--- snake_bounce.py
+++ snake_bounce.py
@@ -1,27 +1,3 @@
-
-##################################################################################################################
-# Edit the sendit function below to replace YOURSERVERGOESHERE with your webs erver or backend server.           #
-#                                                                                                                #
-# Note that snake_bounce.py should basically always get an HTTP 404 when it works...                             #
-#                                                                                                                #
-# The idea is to use this to send the data to a server                                                           #
-# in an HTTPS GET request via data packed into the URI context.                                                  #
-# If the server is setup to read the request data and then                                                       #
-# extract these strings, they can be encrypted server side                                                       #
-# and then stored on the file system or just extracted from                                                      #
-# web logs if you don't have a custom web service to do                                                          #
-# that data manipulation. The logs or custom back-end                                                            #
-# files on the server then can be used to access the data.                                                       #
-#                                                                                                                #
-# As long as your server uses HTTPS and you are not MITM'd with a proxy on a (work) network                      #
-# the data you send will be encrypted in transit. There is also the potential to                                 #
-# do encryption afterwards on the server side with the data, and/or before in                                    #
-# another function, which is demonstrated in the snake_bounce_fernet.py version.                                 #
-#                                                                                                                #
-# Made for either python2 or python3                                                                             #
-# anaconda python has all of these modules ready to go                                                           #
-##################################################################################################################
-
 import base64
 import json
 import urllib
