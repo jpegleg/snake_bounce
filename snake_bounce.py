21199a7a3f9079c7b9067983c65a61ebec8a0f2e
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2020-11-21 04:57:42 -0700

    Update snake_bounce.py

--- snake_bounce.py
+++ snake_bounce.py
@@ -1,31 +1,35 @@
+'''tk GUI EaaS frontend template, tested using anaconda python(2,3)'''
 import base64
 import urllib.request
 from tkinter import Tk, Entry, Button, INSERT
 
-root = Tk()
-entry = Entry(root)
-entry.pack()
-
-entry.insert(INSERT, ' ')
+ROOT = Tk()
+ROOT.title("EaaS API FrontEnd - HTTPS submit")
+ENTRY = Entry(ROOT)
+ENTRY.pack()
+ENTRY.insert(INSERT, ' ')
 
 # function to encode the string for URI portability
 # to deal with spaces and special characters
 def eocde():
-    data = entry.get()
-    encodedBytes = base64.b64encode(data.encode("utf-8"))
-    return str(encodedBytes, "utf-8")
+    '''base64 encode the input box data for transport'''
+    data = ENTRY.get()
+    encbyte = base64.b64encode(data.encode("utf-8"))
+    return str(encbyte, "utf-8")
 
-# function to create the full context using the 
+# function to create the full context using the
 # input string created from eocde function
 def sendit():
+    '''build a URI context for hard-coded URL with encoded data appended to the context'''
     return "https://YOURSERVERGOESHERE/api/{}".format(eocde())
 
 # function to do an HTTP GET with our URL created by sendit function
 def swrap():
+    '''make an HTTP request with urllib for the URI context from sendit function'''
     with urllib.request.urlopen(sendit()) as url:
         url.read()
 
-button = Button(root, text='send content', command=swrap)
-button.pack()
+BUTTON = Button(ROOT, text='send content', command=swrap)
+BUTTON.pack()
 
-root.mainloop()
+ROOT.mainloop()
